8b14c8fe4439ab27fc373babae2e1c02e4666573
Knut Olav Bøhmer <bohmer@gmail.com>
2010-10-22 05:13:39 -0700

    Hopefully reducing memory, ans speeding up even a litle bit more.

--- colorMap.hs
+++ colorMap.hs
@@ -12,7 +12,10 @@ colors :: [Color]
 colors = [Red, Blue, Green, Purple]
 
 -- colorsPermitted: [] means any color from global colors
-data Node = Node {nameOf::Country, colorOf::Color, neighborsOf::[Country], colorsPermitted::[Color] } deriving (Eq, Show, Read)
+data Node = Node {nameOf::Country, 
+                  colorOf::Color, 
+                  neighborsOf::[Country], 
+                  colorsPermitted::[Color] } deriving (Eq, Show, Read)
 
 nameEq :: Country -> Node -> Bool
 nameEq name node = name == (nameOf node)
@@ -25,31 +28,30 @@ getNode graph name = find (nameEq name) graph
 
 -- The program
 
-doColors :: Node -> [Node]
-doColors node@(Node _ _ _ []) = [ node {colorOf = c} | c <- colors ]
-doColors node@(Node _ _ _ colors) = [ node {colorOf = c} | c <- colors ]
+doColors :: Node -> [Color]
+doColors node@(Node _ _ _ []) =     [ c | c <- colors ]
+doColors node@(Node _ _ _ colors) = [ c | c <- colors ]
 
 colorMap :: [Node] -> [[Node]]
 colorMap [] = [[]]
-colorMap (node:restNodes) = [ y:ys | ys <- colorMap restNodes,
-                                     let colorhood = (map colorOf (getNeighbors ys node)),
-                                     y <- doColors node,
-                                     (colorOf y) `notElem`  colorhood ]
+colorMap (node:restNodes) = [ node {colorOf = color}:theMap | theMap <- colorMap restNodes
+                                                            , let colorHood = (map colorOf (getNeighbors theMap node)),
+                                                              color <- doColors node,
+                                                              color `notElem` colorHood ]
 -- Map data
 mapData :: [(Country, [Country], [Color])]
-
-mapData =[(Finland, [Sweden, Norway, Russia], []),
-          (Sweden, [Finland, Norway], []),
-          (Norway, [Finland, Sweden, Russia], []),
-          (Russia, [Estonia, Finland, Latvia, Lithuania, Belarus,
+mapData =[(Finland,     [Sweden, Norway, Russia],  []),
+          (Sweden,      [Finland, Norway],         []),
+          (Norway,      [Finland, Sweden, Russia], []),
+          (Russia,      [Estonia, Finland, Latvia, Lithuania, Belarus,
                          Ukraine, Norway], []),
-          (Estonia, [Russia, Latvia], []),
-          (Latvia, [Russia, Estonia, Lithuania, Belarus], []),
-          (Lithuania, [Latvia, Belarus, Poland, Kaliningrad], []),
+          (Estonia,     [Russia, Latvia],  []),
+          (Latvia,      [Russia, Estonia, Lithuania, Belarus],  []),
+          (Lithuania,   [Latvia, Belarus, Poland, Kaliningrad], []),
           (Kaliningrad, [Lithuania, Poland], []),
-          (Belarus, [Poland, Lithuania, Latvia, Russia, Ukraine], []),
-          (Ukraine, [Poland, Belarus, Russia], []),
-          (Poland, [Kaliningrad, Lithuania, Belarus, Ukraine], [])]
+          (Belarus,     [Poland, Lithuania, Latvia, Russia, Ukraine], []),
+          (Ukraine,     [Poland, Belarus, Russia], []),
+          (Poland,      [Kaliningrad, Lithuania, Belarus, Ukraine],   [])]
 
 mkMap :: [(Country, [Country], [Color])] -> [Node]
 mkMap nodes = [(Node name None neighbors colors) | (name, neighbors, colors) <- nodes]
@@ -71,4 +73,3 @@ printSolution graph = map printMap graph
 --       print (printMap (last (colorMap1 theMap)))
 
 main = do print (printSolution (colorMap theMap))
-
